47b9b69f807aa94964f9cfa576f59d43c32fc83d
Amit biswas <amitbiswas9282@gmail.com>
2025-04-08 21:26:31 +0600

    banner modify

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -6,45 +6,27 @@ import img3 from "../../../../public/banner/3.png";
 import img4 from "../../../../public/banner/4.jpg";
 import img5 from "../../../../public/banner/5.png";
 
+const values = [
+  { image: img1 },
+  { image: img2 },
+  { image: img3 },
+  { image: img4 },
+  { image: img5 },
+];
+
 const Banner = () => {
   return (
     <div className="mt-16">
       <Carousel showThumbs={false} infiniteLoop autoPlay>
-        <div className="h-[540px] w-full">
-          <img
-            src={img1}
-            alt="Image 1"
-            className="h-full w-full object-cover"
-          />
-        </div>
-        <div className="h-[540px] w-full">
-          <img
-            src={img2}
-            alt="Image 2"
-            className="h-full w-full object-cover"
-          />
-        </div>
-        <div className="h-[540px] w-full">
-          <img
-            src={img3}
-            alt="Image 3"
-            className="h-full w-full object-cover"
-          />
-        </div>
-        <div className="h-[540px] w-full">
-          <img
-            src={img4}
-            alt="Image 3"
-            className="h-full w-full object-cover"
-          />
-        </div>
-        <div className="h-[540px] w-full">
-          <img
-            src={img5}
-            alt="Image 3"
-            className="h-full w-full object-cover"
-          />
-        </div>
+        {values.map((value) => (
+          <div className="h-[540px] w-full">
+            <img
+              src={value.image}
+              alt="Image Not Found"
+              className="h-full w-full object-cover"
+            />
+          </div>
+        ))}
       </Carousel>
     </div>
   );
