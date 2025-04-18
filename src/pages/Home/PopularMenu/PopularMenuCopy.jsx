038dcf1e6a7d6e3e64ef493135c6cc02d829e86e
Amit biswas <amitbiswas9282@gmail.com>
2025-04-18 13:14:00 +0600

    popularMenu

---
+++ src/pages/Home/PopularMenu/PopularMenuCopy.jsx
@@ -0,0 +1,40 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const PopularMenuCopy = ({ items, SubHeading, Heading }) => {
+
+  return (
+    <div className="text-black bg-white pb-20 container mx-auto px-3">
+      <SectionTitle subHeading={SubHeading} heading={Heading} />
+      <div className="md:grid md:grid-cols-2 gap-10">
+        {items.map((item) => (
+          <div key={item._id || item.id} className="col-span-1 flex gap-6 mb-6">
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-[60px] h-[60px] bg-[#D9D9D9] rounded-b-full rounded-r-full object-cover"
+            />
+            <div>
+              <div className="flex justify-between items-center my-2">
+                <h2 className="text-xl font-semibold text-[#151515] uppercase">
+                  {item.name}{" "}
+                  <span className="text-[#d3d3d3]">------------------</span>
+                </h2>
+                <p className="text-xl font-semibold text-[#151515] text-end">
+                  ${item.price}
+                </p>
+              </div>
+              <p className="text-[#737373] max-w-[550px]">{item.recipe}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="text-center">
+        <button className="my-6 btn border-b-2 border-l-0 border-r-0 border-t-0 bg-none border-white">
+          View Full Menu
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PopularMenuCopy;
--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,46 +1,74 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../Shared/Cover/Cover";
-import img1 from "../../../public/menu2/banner3.jpg"
-import img2 from "../../../public/menu2/dessert-bg.jpeg"
-import img3 from "../../../public/menu2/pizza-bg.jpg"
-import img4 from "../../../public/menu2/salad-bg.jpg"
-import img5 from "../../../public/menu2/soup-bg.jpg"
+import img1 from "../../../public/menu2/banner3.jpg";
+import img2 from "../../../public/menu2/dessert-bg.jpeg";
+import img3 from "../../../public/menu2/pizza-bg.jpg";
+import img4 from "../../../public/menu2/salad-bg.jpg";
+import img5 from "../../../public/menu2/soup-bg.jpg";
+import { useEffect, useState } from "react";
+import PopularMenuCopy from "../Home/PopularMenu/PopularMenuCopy";
 
 const items = [
   {
     id: 1,
-    img : img1,
-    title : "OUR MENU",
-    description: "Would you like to try a dish?"
+    img: img1,
+    title: "OUR MENU",
+    description: "Would you like to try a dish?",
   },
   {
     id: 2,
-    img : img2,
-    title : "DESSERTS",
-    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    img: img2,
+    title: "DESSERTS",
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
   },
   {
     id: 3,
-    img : img3,
-    title : "PIZZA",
-    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    img: img3,
+    title: "PIZZA",
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
   },
   {
     id: 4,
-    img : img4,
-    title : "SALADS",
-    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    img: img4,
+    title: "SALADS",
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
   },
   {
     id: 5,
-    img : img5,
-    title : "SOUPS",
-    description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    img: img5,
+    title: "SOUPS",
+    description:
+      "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
   },
-]
+];
 
 const Menu = () => {
-  const coverItem = items[0];
+  const [popularMenu, setPopularMenu] = useState([]);
+  const [pizzaMenu, setPizzaMenu] = useState([]);
+  const [saladMenu, setSaladMenu] = useState([]);
+  const [dessertMenu, setDessertMenu] = useState([]);
+  const [soupMenu, setSoupMenu] = useState([]);
+
+  const coverItem1 = items[0];
+  const coverItem2 = items[1];
+  const coverItem3 = items[2];
+  const coverItem4 = items[3];
+  const coverItem5 = items[4];
+
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setPopularMenu(data.filter((item) => item.category === "popular"));
+        setPizzaMenu(data.filter((item) => item.category === "pizza"));
+        setSaladMenu(data.filter((item) => item.category === "salad"));
+        setDessertMenu(data.filter((item) => item.category === "dessert"));
+        setSoupMenu(data.filter((item) => item.category === "soup"));
+      });
+  }, []);
 
   return (
     <div className="mt-16">
@@ -48,7 +76,16 @@ const Menu = () => {
         <title>bisTro Boss || Menu</title>
         <meta name="description" content="This is the home page" />
       </Helmet>
-      <Cover img={coverItem.img} title={coverItem.title} description={coverItem.description}/>
+      <Cover
+        img={coverItem1.img}
+        title={coverItem1.title}
+        description={coverItem1.description}
+      />
+      <PopularMenuCopy
+        items={popularMenu}
+        SubHeading={`Don't miss`}
+        Heading={`TODAY'S OFFER`}
+      />
     </div>
   );
 };
